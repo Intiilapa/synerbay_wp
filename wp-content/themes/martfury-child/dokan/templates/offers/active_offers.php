14635d3457133104fe41a64ee2bf5c5e2156d53e
Nagy Kristóf <kristof.nagy@w5labs.com>
2020-12-13 22:08:21 +0100

    dashboard fix offer links

--- wp-content/themes/martfury-child/dokan/templates/offers/active_offers.php
+++ wp-content/themes/martfury-child/dokan/templates/offers/active_offers.php
@@ -5,6 +5,7 @@ global $myOfferApplies;
 ?>
 <h1>Active offers</h1>
 <hr>
+
 <table class="dokan-table dokan-table-striped product-listing-table dokan-inline-editable-table" id="dokan-product-list-table">
     <thead>
     <tr>
@@ -12,7 +13,7 @@ global $myOfferApplies;
         <th><?php esc_html_e( 'Offer Name', 'dokan-lite' ); ?></th>
         <th><?php esc_html_e( 'Quantity', 'dokan-lite' ); ?></th>
         <th><?php esc_html_e( 'Current Price', 'dokan-lite' ); ?></th>
-        <th><?php esc_html_e( 'Applicant Product Number', 'dokan-lite' ); ?></th>
+        <th><?php esc_html_e( 'Current quantity', 'dokan-lite' ); ?></th>
         <th><?php esc_html_e( '', 'dokan-lite' ); ?></th>
         <th><?php esc_html_e( 'Actions', 'dokan-lite' ); ?></th>
     </tr>
@@ -25,11 +26,18 @@ foreach ($myOfferApplies as $offerApply) {
         . '<td>'. $offerApply['qty'] . '</td>'
         . '<td>'. $offerApply['offer']['summary']['formatted_actual_product_price'] . '</td>'
         . '<td>'. $offerApply['offer']['summary']['actual_applicant_product_number'] . '</td>'
-        . '<td>'.'<a href="stage.synerbay.com/offer/' . $offerApply['offer_id'] . '">Details</a></td>'
+        . '<td>'.'<a target="_blank" href="' . $offerApply['offer']['url'] . '">Details</a></td>'
         . '<td>'."<a onclick='window.synerbay.disAppearOffer(".$offerApply['offer_id'].")' class='dokan-btn dokan-btn-default dokan-btn-sm tips'>x</a>".'</td>'
         . '</tr>';
 }
-//do_action('synerbay_disAppearOfferDashBoardButton', $offerApply['offer_id'])
+
+if (!$myOfferApplies){
+    echo  '<td colspan="7">No active offers found</td>';
+}
+//do_action("synerbay_disAppearOfferDashBoardButton', $offerApply['offer_id'])
 ?>
+
 </thead>
 </table>
+
+<button onClick="window.location.reload();">Refresh active offers</button>
