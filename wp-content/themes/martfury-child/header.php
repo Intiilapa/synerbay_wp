efb9e0968c21d45829ef14d56cebe88268efef59
Remco <remcoad@gmail.com>
2021-04-20 09:26:31 +0200

    Gitfix?

--- wp-content/themes/martfury-child/header.php
+++ wp-content/themes/martfury-child/header.php
@@ -14,14 +14,25 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-<!--    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MK9E3HHMWV"></script>-->
-<!--    <script>-->
-<!--        window.dataLayer = window.dataLayer || [];-->
-<!--        function gtag(){dataLayer.push(arguments);}-->
-<!--        gtag('js', new Date());-->
-<!---->
-<!--        gtag('config', 'G-MK9E3HHMWV');-->
-<!--    </script>-->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MK9E3HHMWV"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-MK9E3HHMWV');
+    </script>
+
+    <!-- GetSiteControl -->
+    <script type="text/javascript" async src="//l.getsitecontrol.com/3w06pm5w.js"></script>
+
+    <!-- Iubenda -->
+    <script type="text/javascript">
+        var _iub = _iub || [];
+        _iub.csConfiguration = {"consentOnContinuedBrowsing":false,"lang":"en","siteId":2160664,"cookiePolicyId":51446906, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"acceptButtonColor":"#ff004e","acceptButtonCaptionColor":"white","customizeButtonColor":"#DADADA","customizeButtonCaptionColor":"#4D4D4D","rejectButtonDisplay":true,"rejectButtonColor":"#ff004e","rejectButtonCaptionColor":"white","position":"float-bottom-left","textColor":"black","backgroundColor":"white" }};
+    </script>
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <script src="https://kit.fontawesome.com/066facd3ea.js" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
