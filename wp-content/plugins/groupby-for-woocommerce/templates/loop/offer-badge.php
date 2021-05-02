f13699d90476a9cfecd5283da091b662768bf15d
Remco <remcoad@gmail.com>
2021-05-02 19:20:36 +0200

    offer ended card notification

--- wp-content/plugins/groupby-for-woocommerce/templates/loop/offer-badge.php
+++ wp-content/plugins/groupby-for-woocommerce/templates/loop/offer-badge.php
@@ -2,15 +2,20 @@
 /**
  * Group Buy badge template
  *
- * @author 	WooThemes
- * @package 	WooCommerce/Templates
+ * @author    WooThemes
+ * @package    WooCommerce/Templates
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-global $product;
+global $product, $offer;
+$time_remaining = strtotime($offer['offer_end_date']) - (get_option('gmt_offset') * 3600);
+$time_for_start = strtotime($offer['offer_start_date']) - (get_option('gmt_offset') * 3600);
+$currentDate = strtotime(date('Y-m-d H:i:s'));
 
-?>
-
-<?php echo apply_filters('woocommerce_groupbuy_bage', '<span class="groupbuy-bage dashicons dashicons-groups"></span>',  $product); ?>
+if ($currentDate > $time_remaining)
+   echo apply_filters('woocommerce_groupbuy_bage', '<span class="groupbuy-bage dashicons dashicons-yes-alt"></span>', $product);
+else {
+  echo apply_filters('woocommerce_groupbuy_bage', '<span class="groupbuy-bage dashicons dashicons-groups"></span>', $product);
+}
--- wp-content/plugins/groupby-for-woocommerce/templates/loop/offer-countdown.php
+++ wp-content/plugins/groupby-for-woocommerce/templates/loop/offer-countdown.php
@@ -10,31 +10,35 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product, $offer;
-
 $time_remaining = strtotime($offer['offer_end_date'])-  (get_option( 'gmt_offset' )*3600);
 $time_for_start = strtotime($offer['offer_start_date'])-  (get_option( 'gmt_offset' )*3600);
-
 $currentDate = strtotime(date('Y-m-d H:i:s'));
-
 ?>
+
+
 <?php if($offer['summary']['actual_product_price'] != 0): ?>
     <p class="price">
         <?php echo $offer['summary']['formatted_actual_product_price'];?>
     </p>
 <?php endif;?>
 
-<?php if($currentDate > strtotime($offer['offer_start_date'])) : ?>
+<?php if ($currentDate > $time_remaining): ?>
+    <!-- Offer ended -->
+    <p class="offer-ended-view">This offer has ended</p>
 
+<?php elseif ($currentDate < $time_for_start) : ?>
+    <!-- Offer starts in  -->
+    <div class="groupbuy-time future" id="countdown"><?php echo  __( 'Offer starts in:', 'wc_groupbuy' ) ?>
+        <div class="groupbuy-time-countdown future" data-time="<?php echo $time_for_start ?>" data-format="<?php echo get_option( 'simple_groupbuy_countdown_format' ) ?>"></div>
+    </div>
+
+<?php else: ?>
+    <!-- Active offer -->
     <div class='groupbuy-ajax-change'>
         <div class="groupbuy-time" id="countdown"><?php echo apply_filters('time_text', __( 'Time left:', 'wc_groupbuy' ), $product); ?>
             <div class="main-groupbuy groupbuy-time-countdown" data-time="<?php echo $time_remaining ?>" data-groupbuyid="<?php echo $product->get_id() ?>" data-format="<?php echo get_option( 'simple_groupbuy_countdown_format' ) ?>"></div>
         </div>
     </div>
 
-<?php elseif($currentDate < strtotime($offer['offer_start_date'])) : ?>
+<?php endif;?>
 
-    <div class="groupbuy-time future" id="countdown"><?php echo  __( 'Offer starts in:', 'wc_groupbuy' ) ?>
-        <div class="groupbuy-time-countdown future" data-time="<?php echo $time_for_start ?>" data-format="<?php echo get_option( 'simple_groupbuy_countdown_format' ) ?>"></div>
-    </div>
-
-<?php endif; ?>
